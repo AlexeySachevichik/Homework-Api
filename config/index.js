e86de3a412acb860d8b9b0bc018140d4baa227bc
Alexey Sachevichik <alexey.sachevichik@succraft.com>
2019-11-14 17:54:30 +0300

    Add .env

--- config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	"localuri": 'mongodb://localhost:27017/basebike',
-	"uri": "mongodb+srv://verespro:vQyZp0JrqMSpSH1P@bike-fdllb.mongodb.net/test?retryWrites=true",
-    "port": 8000,
-    "database": 'database_bike'
-};
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const config         = require("./config");
-const func           = require("./func");
-
-const expressClass   = require("express");
-const bodyParser     = require("body-parser");
-const mongo          = require("mongodb").MongoClient;
-const client         = new mongo(config.uri, { useNewUrlParser: true });
-const app            = expressClass();
+// EXPRESS SERVER
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const func = require('./function'); 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,14 +13,34 @@ app.use(function (req, res, next) {
     next();
 });
 
-client.connect( function(error, database){
-	if(error) return console.log(error);
-	
-	const db = database.db(config.database);
 
-	app.listen(config.port, (error) => {
-		if(error) return console.log(error);
-		require("./routes")(app, db);
-		func.mes(`Start listen in: http://localhost:${config.port}`);
-	});
+// ENVIROMENT
+require('dotenv').config();
+const PORT = process.env.PORT;
+const MONGO_DB = process.env.MONGO_DB;
+
+
+// MONGO
+const MONGO_URL = 'mongodb+srv://' + process.env.MONGO_USER + ':' + process.env.MONGO_KEY + '.mongodb.net/test?retryWrites=true';
+const MONGO = require("mongodb").MongoClient;
+const client = new MONGO(MONGO_URL, { useNewUrlParser: true });
+
+
+// START LISTENING SERVER
+app.listen(PORT, (error) => {
+	func.mes('[START] Server start listening on port ' + PORT);
 });
+
+
+
+// client.connect( function(error, database){
+// 	if (error) return console.log(error);
+	
+// 	const db = database.db(config.database);
+
+// 	app.listen(PORT, (error) => {
+// 		if(error) return console.log(error);
+// 		require("./routes")(app, db);
+// 		func.mes(`Start listen in: http://localhost:${config.port}`);
+// 	});
+// });
